daa371c503761a00427773064de591e5636dafc3
Mohd Arif <beestrides@gmail.com>
2025-07-18 08:34:13 +0000

    There are many whitespaces left before the new section, please resolve t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,7 +96,7 @@ export default function HomePage() {
             className="object-cover"
           />
           <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
-          <div className="absolute inset-0 container px-4 md:px-6 flex flex-col justify-end pb-12 md:pb-24">
+          <div className="absolute inset-0 container flex flex-col justify-end pb-12 md:pb-24">
             <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-white font-headline">
               Discover Timeless Furniture for Every Space
             </h1>
